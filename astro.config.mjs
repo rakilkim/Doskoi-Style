9fe7e1d5484a913bb6839a6c8fd8700fbae037ef
Rakil Kim <rakilkim0216@gmail.com>
2026-09-18 20:46:24 -0700

    remove node adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 
 export default defineConfig({
   output: "static",
-  adapter: node({
-    mode: "standalone"
-  }),
   site: 'https://rakilkim.github.io',
   base: '/Doskoi-Style',
 });
